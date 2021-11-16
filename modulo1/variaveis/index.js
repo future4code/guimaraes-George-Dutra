5f2de728db9f1f96c5d6a1b0eeb77545f910512f
George Saraiva <georgesaraiva@gmail.com>
2021-11-15 23:10:35 -0300

    exercicio-de-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,92 @@
+//EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO
+// 1.
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+//RESPOSTAS:
+// 10; 10 e 5;
+
+// 2.
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+//RESPOSTAS:
+// 10; 10; 10
+
+// // 3.
+// let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+// let valorDiaria = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+//RESPOSTA:
+
+// O PROGRAMA EXIBE UMA PERGUNTA AO USUÁRIO ATRAVÉS DE "prompt de comando"
+//NOMES PARA AS VARIÁVEIS:
+// p = horasTrabalhada
+// t = valorDiaria
+
+//Exercícios de escrita de código
+// 1.
+
+// const nome = ("George")
+// let idade = 38
+
+// console.log (typeof nome, typeof idade)
+// NA TELA FOI IMPRESSO um ERRO DE: "Uncaught SyntaxError: Unexpected identifier"
+// QUANDO ADICIONEI VALORES FOI IMPRESSO: "string number"
+
+// e) Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+
+// const nome = prompt ("Qual o seu nome?")
+// const idade =  prompt ("Qual a sua idade?")
+
+// console.log ("Olá ", nome, "você tem ", idade, "anos!" )
+
+// console.log (typeof nome, typeof idade)
+
+//NÃO RECEBI MENSAGEM DE ERROS!;
+// O CONSOLE IMPRIMIU "STRING, STRING";
+
+// Escreva um programa que faça 3 perguntas de Sim ou Não, armazenado em uma variável. Por exemplo: "Você está usando uma roupa azul hoje?". Depois, siga os passos:
+//RESPOSTAS:
+
+// const A = prompt ("Você gosta de estudar?")
+// const B = prompt ("Você se exercita")
+// const C = prompt ("Você gosta de café")
+
+// const A1 = true
+// const B2= true 
+// const C3 = true
+
+// console.log (A, B, C)
+
+// EXERCICIO 3.
+
+let a = 10
+let b = 25
+let c = a
+
+//RESOLUÇÃO:
+// a = c
+// b = a
+// c = b
+
+console.log("O novo valor de a é", b) 
+console.log("O novo valor de b é", a) 
+
+
+//ESTE EXERCICIO EU NÃO CONSEGUI ENTENDER A RESOLUÇÃO, MESMO OLHANDO A RESOLUÇÃO DA INSTRUTORA CHIJO.
+//MAS TENTEI E SAIU ESSE RESULTADO, MAS CONTINUA SEM FAZER SENTIDO PRA MIM!
+
+
+//FIM.
